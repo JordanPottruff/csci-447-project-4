a27be3e4712d4c47977214e5cbd06846ae2395b3
jordanpottruff <jordanpottruff@gmail.com>
2019-12-05 14:24:57 -0700

    Add comments to particle swarm file.

--- src/training/particle_swarm.py
+++ src/training/particle_swarm.py
@@ -20,6 +20,7 @@ class ParticleSwarm:
         self.convergence_size = convergence_size
         self.particles = [Particle(network, max_velocity) for i in range(pop_size)]
 
+    # Returns the particle with the current best state among all the particles.
     def __get_global_best(self):
         most_fit_particle = self.particles[0]
         highest_fitness = self.particles[0].get_fitness()
@@ -30,6 +31,10 @@ class ParticleSwarm:
                 highest_fitness = particle_fitness
         return most_fit_particle
 
+    # Begins the training process. Moves each particle according to its velocity, and then updates the velocity of each
+    # particle.
+    # TODO: train needs to update the network's weights with the best weights found.
+    # TODO: add convergence check (use convergence_size parameter) similar to backprop.
     def train(self):
         for i in range(100):
             g_best = self.__get_global_best()
@@ -37,18 +42,19 @@ class ParticleSwarm:
             for particle in self.particles:
                 particle.move()
                 particle.update_velocity(self.cog_factor, self.soc_factor, self.inertia, g_best.state)
-        pass
 
 
 LOWER_BOUND = -100000
 UPPER_BOUND = 100000
 
 
+# Particle class that contains a lot of the central logic for the particle swarm technique.
 class Particle:
 
+    # Creates a new particle. Requires a network object in order to generate the right (random) vector representing the
+    # weights. Also requires a max velocity parameter that will limit how fast a particle moves through the state space.
     def __init__(self, network: Network, max_velocity: float):
         self.network = network
-        self.data_set = network.training_data
         self.max_velocity = max_velocity
         self.state = np.random.uniform(low=LOWER_BOUND, high=UPPER_BOUND, size=network.get_num_weights())
         self.velocity = np.random.uniform(low=-1, high=1, size=network.get_num_weights())
@@ -56,28 +62,33 @@ class Particle:
         self.p_best = self.state
         self.best_fitness = self.get_fitness()
 
+    # Returns a string representation of the particle, showing the state and velocity.
     def __str__(self):
         return "Particle{state: " + str(self.state) + ",\nvelocity: " + str(self.velocity) + "}"
 
-    def encode(self, weight_shapes: list):
+    # Turns the current particle into a list of weight matrices to use in a network.
+    def encode(self):
         weights = []
         i = 0
-        for shape in weight_shapes:
+        for shape in self.network.get_weight_shapes():
             size = reduce((lambda x, y: x * y), shape)
             weights.append(np.reshape(self.state[i:i+size], shape))
             i += size
         return weights
 
+    # Returns the fitness of the particle's current state. The fitness is evaluated on the training set for the network.
+    # The fitness is accuracy if a classification problem, and the inverse error for regression.
     def get_fitness(self):
         old_weights = self.network.weights
-        self.network.weights = self.encode(self.network.get_weight_shapes())
+        self.network.weights = self.encode()
         if self.network.is_regression():
-            fitness = 1/self.network.get_error(self.data_set)
+            fitness = 1/self.network.get_error(self.network.training_data)
         else:
-            fitness = self.network.get_accuracy(self.data_set)
+            fitness = self.network.get_accuracy(self.network.training_data)
         self.network.weights = old_weights
         return fitness
 
+    # Moves the particle from the current state to a new state based on the current velocity.
     def move(self):
         # print("velocity=" + str(np.linalg.norm(self.velocity)))
         self.state += self.velocity
@@ -86,6 +97,7 @@ class Particle:
             self.p_best = self.state
             self.best_fitness = new_fitness
 
+    # Updates the velocity based on the gbest particle swarm technique.
     def update_velocity(self, cog_factor: float, soc_factor: float, inertia: float, g_best: np.ndarray):
         cognitive = random.random() * cog_factor * (self.p_best - self.state)
         social = random.random() * soc_factor * (g_best - self.state)
@@ -104,6 +116,7 @@ def test_particle_swarm_car():
     # [print(weight) for weight in a.encode([(3,4), (2, 3), (2, 2)])]
     b = ParticleSwarm(net, pop_size=100, cog_factor=0.01, soc_factor=0.05, inertia=0.05, max_velocity=100000, convergence_size=100)
     b.train()
+
 
 def test_particle_swarm_machine():
     data = ds.get_machine_data("../../data/machine.data")
