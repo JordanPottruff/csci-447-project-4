eb8bfda2d0a2cb4316d5c043aae3987140642916
Jacob Weikert <Weikjaco@gmail.com>
2019-12-08 14:59:27 -0700

    Update diff_evolution.py

--- src/training/diff_evolution.py
+++ src/training/diff_evolution.py
@@ -7,25 +7,49 @@ import random as rd
 import src.util.activations as af
 from src.network import Network
 from src.data import *
+from functools import reduce
 
 class DiffEvolution:
-    # DE is a population based optimizer that perturbs vectors using scaled differences of two randomly generated population vectors
+    # DE is a population based optimizer that perturbs vectors using scaled differences of randomly generated
+    # individual vectors
 
-    def __init__(self, network: Network, bounds: list, mutationF: float, recombinationC: float):
+    def __init__(self, network: Network, mutationF: float, recombinationC: float, popsize=30):
         self.network = network  # Minimize the objective function by optimizing the values of the network weights
-        self.bounds = bounds  # Boundary to limit search space, speeding up algorithm
         self.mutationF = mutationF  # Mutation rate
         self.recombinationC = recombinationC  # Recombination rate
-        self.weight_size = self.network.get_num_weights()  # Size of desired population
-        self.population = np.random.uniform(low=bounds[0], high=bounds[1], size=self.weight_size)  # Initialize Population
+        self.population_size = popsize
+        self.population = self.initialize_population()  # Initialize Population
+        self.run_nn_on_pop_weights()
         self.test_pop = self.population  # used so we can compare old weights with new weights
+        self.individual_feature_size = self.network.get_num_weights()  # Individuals features
+
         self.final_performance = 0
         self.run()
+
+    def run_nn_on_pop_weights(self):
+        fitness = []
+        cnt = 0
+        for individual in self.population:
+            self.network.weights = individual
+            fitness.append(self.get_fitness())
+            cnt += 1
+        print(fitness)
+
+    def initialize_population(self):
+        bounds = -2000, 2000
+        population = []
+        print("Individual Feature Size" + str(self.individual_feature_size))
+        for individual in range(self.population_size):
+            individual = np.random.uniform(low=bounds[0], high=bounds[1],
+                                            size=self.individual_feature_size)  # Create Individual
+            population.append(individual)  # Add individual to population
+        return population
+
 
     def mutation(self, loc):
         """Mutation is used to allow us to explore our space to find a good solution. To do this we select three
         vectors at random from the current population."""
-        # Get random index's were changing
+        # Get random index's of the population were going changing
         mutation_idx = []
         for _ in range(3):
             value = rd.sample(range(0, len(self.population)-1), 3)
@@ -34,14 +58,20 @@ class DiffEvolution:
             mutation_idx.append(value)
 
         # Mutate them
-        mutant = mutation_idx[0] + self.mutationF * (mutation_idx[1] - mutation_idx[2])
+        print("Mutation Idx: " + str(mutation_idx))
+        print("Mutation Idx Type: " + type(mutation_idx))
+        first_chosen_one = np.asarray(self.population[mutation_idx[0]])
+        second_chosen_one = np.asarray(self.population[mutation_idx[1]])
+        third_chosen_one = np.asarray(self.population[mutation_idx[2]])
+        mutant = first_chosen_one + self.mutationF * (second_chosen_one - third_chosen_one)
         return mutant
 
     def recombination(self, loc, mutant):
         """Do we perform crossover"""
-        we_do_replace = random.uniform(0, 1) < self.recombinationC
-        if we_do_replace:
-            self.test_pop[loc] = mutant
+        for i in range(self.individual_feature_size):
+            we_do_replace = random.uniform(0, 1) < self.recombinationC
+            if we_do_replace:
+                self.test_pop[loc][i] = mutant[i]
 
     def run(self):
         iteration = 1000
@@ -49,12 +79,10 @@ class DiffEvolution:
             for loc in range(self.population - 1):  # For each location
                 mutant = self.mutation(loc)  # Mutate
                 self.recombination(loc, mutant)  # Perform crossover
-                #TODO run neural network with new weights and see if it performs better than old.
-                # If yes than keep new weights, else discard weights
                 # update weights of network
-                old_performance = self.network.run()
+                old_performance = self.get_fitness()
                 self.network.weights = self.test_pop  # Test with new weights
-                new_performance = self.network.run()
+                new_performance = self.get_fitness()
                 self.network.weights = self.population  # Change back just in case
 
                 if old_performance < new_performance:  # if performance better we replace the population with the mutant
@@ -64,22 +92,29 @@ class DiffEvolution:
                 self.final_performance = old_performance
         return self.final_performance
 
+    def encode(self):
+        weights = []
+        i = 0
+        for shape in self.network.get_weight_shapes():
+            size = reduce((lambda x, y: x * y), shape)
+            weights.append(np.reshape(self.state[i:i + size], shape))
+            i += size
+        return weights
+
+    # Returns the fitness of the individuals current state. The fitness is evaluated on the training set for the network.
+    # The fitness is accuracy if a classification problem, and the inverse error for regression.
+    def get_fitness(self):
+        old_weights = self.network.weights #TODO update to work with your code
+        self.network.weights = self.encode()
+        if self.network.is_regression():
+            fitness = 1 / self.network.get_error(self.network.training_data)
+        else:
+            fitness = self.network.get_accuracy(self.network.training_data)
+        self.network.weights = old_weights
+        return fitness
+
     def weights_to_vector(self):
         pass
-def get_bounds(network):
-    """Returns the lower and upper bounds of the weight parameters that are going to be passed into differential
-    evolution. This will reduce our search space... in a good way?"""
-    max_value = 0
-    min_value = 10000
-    for weight_layer in network.weights:
-        for all_weights in weight_layer:
-            for weight in all_weights:
-                if weight > max_value:
-                    max_value = weight
-                if weight < min_value:
-                    min_value = weight
-    boundary = [min_value, max_value]
-    return boundary
 
 
 dataset = get_machine_data()
@@ -87,6 +122,5 @@ trainset, testset = dataset.partition(.80)
 #print(trainset)
 # test with number of features as size of input layer, guessing for hidden, and 1 output node size as regression is used
 n = Network(trainset, testset, [6, 3, 3, 3, 1])
-bounds = get_bounds(n)
-x = DiffEvolution(n, bounds, .1, .9)
+x = DiffEvolution(n, .1, .9, 39)
 
